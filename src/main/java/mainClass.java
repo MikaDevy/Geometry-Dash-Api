3f9674b8f9ee06b3b30776b84191c983f5395024
MikaDevy <62851960+MikaDevy@users.noreply.github.com>
2020-08-12 10:49:05 -0400

    Thing
    :3

---
+++ src/main/java/mainClass.java
@@ -0,0 +1,21 @@
+public enum forms {
+  cube,
+  ship,
+  ball,
+  ufo,
+  wave,
+  robot,
+  spider
+}
+
+public class mainClass {
+
+  public static forms;
+
+  public static void main(String[] args) {
+    if (!forms) {
+      Debug.Log("Hello white");
+    }
+  }
+
+}
